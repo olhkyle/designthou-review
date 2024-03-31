81886c92a93c8978ba16282b4cb1f45b107d3083
olhkyle <kylekwon96@gmail.com>
2024-03-31 14:36:49 +0900

    [CHORE] add supabase

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'designthou.com',
+				pathname: '/wp-content/uploads/kboard_attached',
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
