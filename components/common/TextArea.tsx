622ecfa31a306db10952f3d47b3dbe140bb80931
olhkyle <kylekwon96@gmail.com>
2024-05-07 18:41:28 +0900

    [FEAT] update TextArea Component's prop

--- components/common/TextArea.tsx
+++ components/common/TextArea.tsx
@@ -7,9 +7,16 @@ interface TextAreaProps {
 	setContent?: Dispatch<SetStateAction<string>>;
 	placeholder: string;
 	width?: string;
+	eventHandler?: () => void;
 }
 
-export default function TextArea({ content, setContent = () => {}, placeholder, width = 'w-full' }: TextAreaProps) {
+export default function TextArea({
+	content,
+	setContent = () => {},
+	placeholder,
+	width = 'w-full',
+	eventHandler,
+}: TextAreaProps) {
 	const targetRef = useRef<HTMLTextAreaElement | null>(null);
 
 	return (
@@ -19,14 +26,22 @@ export default function TextArea({ content, setContent = () => {}, placeholder, 
 			placeholder={placeholder}
 			maxLength={500}
 			rows={1}
-			className={`px-4 py-2 ${width} placeholder:text-gray-500 border-[1px] border-gray-200 rounded-lg outline-offset-2 appearance-none focus:outline-2 focus:outline-rose-200 resize-none overflow-hidden hover:bg-gray-100`}
+			className={`px-4 py-2 ${width} placeholder:text-gray-500 border-[1px] border-gray-200 rounded-lg outline-offset-2 appearance-none cursor-pointer focus:outline-2 focus:outline-rose-200 resize-none overflow-hidden hover:bg-gray-100`}
 			onChange={e => {
+				if (!eventHandler) return;
+				eventHandler();
+
 				setContent(e.currentTarget.value);
 
 				if (targetRef && targetRef.current) {
 					targetRef.current.style.height = 'auto';
 					targetRef.current.style.height = targetRef.current.scrollHeight + 'px';
 				}
+			}}
+			onClick={() => {
+				if (!eventHandler) return;
+
+				eventHandler();
 			}}
 		/>
 	);
