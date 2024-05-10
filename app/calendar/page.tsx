91136090e8bfd536e34818b8a40a05a611513b69
olhkyle <kylekwon96@gmail.com>
2024-05-10 17:09:58 +0900

    [FEAT] add RecruitCalendar page with Calendar UI

---
+++ app/calendar/page.tsx
@@ -0,0 +1,9 @@
+import { CalendarPicker } from '@/components';
+
+export default function Page() {
+	return (
+		<>
+			<CalendarPicker />
+		</>
+	);
+}
--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -2,15 +2,20 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 import { useRecoilState } from 'recoil';
+import { toast } from 'react-toastify';
+import { BsCalendar2Check } from 'react-icons/bs';
 import { Button } from '.';
 import userState from '@/recoil/atom/userState';
 import useClickOutside from '@/hooks/useClickOutside';
+import route from '@/constants/route';
 
 export default function Nav() {
 	const router = useRouter();
+	const pathname = usePathname();
+
 	const [user, setUser] = useRecoilState(userState);
 	const [isClient, setIsClient] = React.useState(false);
 	const [isDropdownActive, setDropdownActive] = React.useState(false);
@@ -26,50 +31,60 @@ export default function Nav() {
 		<>
 			<nav className="sticky top-0 px-[1rem] backdrop-blur sm:bg-white dark:sm:bg-dark sm:backdrop-blur-none z-40">
 				<div className="flex justify-between items-center mx-auto h-[80px]">
-					<h1 className="flex-center min-w-[130px] h-[60px] cursor-pointer">
-						<Link href="/">
-							<Image src="/logo.png" alt="logo" width={200} height={200} priority />
+					<h1 className="flex-center min-w-[180px] h-[60px] cursor-pointer">
+						<Link href={route.HOME}>
+							<Image src="/logo.png" alt="logo" width={180} height={60} priority />
 						</Link>
 					</h1>
 
-					<div className="relative inline-flex justify-end" ref={containerRef}>
-						<Button
-							type={'button'}
-							className={`inline-flex gap-2 text-xs text-white hover:bg-gray-800 sm:text-sm ${
-								isClient ? 'bg-dark' : 'bg-gray-500'
-							}`}
-							onClick={() => {
-								if (user) {
-									setDropdownActive(isDropdownActive => !isDropdownActive);
-								} else {
-									router.push('/auth');
-								}
-							}}>
-							<span>{isClient && user ? user?.username : '인증하기'}</span>
-						</Button>
-						<div
-							className={`${
-								isDropdownActive ? 'absolute' : 'hidden'
-							} top-10 right-0 flex flex-col items-center p-2 w-[100px] bg-gray-100 rounded-md border-[1px] border-gray-300`}>
+					<div className="inline-flex justify-end items-center">
+						<Link
+							href="/calendar"
+							className="relative mr-4 p-2 border-[1px] border-gray-50 bg-white rounded-lg hover:bg-gray-50">
+							<BsCalendar2Check size={24} />
+							<span className="animate-pulse absolute -top-1 -right-1 w-3 h-3 inline-block rounded-full bg-blue-400 " />
+						</Link>
+						<div className="relative" ref={containerRef}>
 							<Button
-								type="button"
-								className="font-normal text-sm text-dark hover:bg-gray-400 hover:font-semibold"
+								type={'button'}
+								className={`${
+									pathname === route.AUTH ? 'hidden' : 'inline-flex'
+								} gap-2 text-xs text-white hover:bg-gray-800 sm:text-sm ${isClient ? 'bg-dark' : 'bg-gray-500'}`}
 								onClick={() => {
-									setDropdownActive(false);
-									router.push(`/mypage/${user?.userId}`);
+									if (user) {
+										setDropdownActive(isDropdownActive => !isDropdownActive);
+									} else {
+										router.push(route.AUTH);
+									}
 								}}>
-								내 정보
+								<span>{isClient && user ? user?.username : '인증하기'}</span>
 							</Button>
-							<Button
-								type="button"
-								className="font-normal text-sm text-dark hover:bg-gray-400 hover:font-semibold"
-								onClick={() => {
-									setUser(null);
-									setDropdownActive(false);
-									router.push('/');
-								}}>
-								로그아웃
-							</Button>
+							<div
+								className={`${
+									isDropdownActive ? 'absolute' : 'hidden'
+								} top-10 right-0 flex flex-col items-center p-2 w-[100px] bg-gray-100 rounded-md border-[1px] border-gray-300`}>
+								<Button
+									type="button"
+									className="font-normal text-sm text-dark hover:bg-gray-400 hover:font-semibold"
+									onClick={() => {
+										setDropdownActive(false);
+										router.push(`${route.MYPAGE}/${user?.userId}`);
+									}}>
+									내 정보
+								</Button>
+								<Button
+									type="button"
+									className="font-normal text-sm text-dark hover:bg-gray-400 hover:font-semibold"
+									onClick={() => {
+										setUser(null);
+										setDropdownActive(false);
+
+										router.push(route.HOME);
+										toast.info('로그아웃 되었습니다');
+									}}>
+									로그아웃
+								</Button>
+							</div>
 						</div>
 					</div>
 				</div>
