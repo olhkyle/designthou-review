9f5517b701c93c7e73f8655c94d824ae5a53ffed
olhkyle <kylekwon96@gmail.com>
2024-05-10 17:08:06 +0900

    [STYLE] change padding of Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { createClient } from '@/supabase/server';
 import Link from 'next/link';
+import Image from 'next/image';
+import { createClient } from '@/supabase/server';
 import { ReviewRegister } from '@/components';
 
 export default async function Home() {
@@ -10,17 +11,23 @@ export default async function Home() {
 	return (
 		<>
 			<ReviewRegister />
-			<ul className="flex flex-col gap-4 mt-10">
+			<ul className="flex flex-col gap-4 my-10">
 				{reviews?.slice(0, 10).map(({ id, username, title, content, imgSrc }) => (
 					<li
 						key={id}
 						className="p-4 border border-gray-200 rounded-xl outline-rose-200 outline-2 outline-offset-2 hover:outline active:outline cursor-pointer transition-all">
-						<Link href={`/reviews/${id}`} className="flex flex-col gap-8 justify-between sm:flex-row">
-							<div className="w-[300px] sm:w-[400px] rounded-lg">
-								<img src={imgSrc} alt={`${username}_${title}`} className="block w-full bg-cover bg-center" />
-							</div>
+						<Link href={`/reviews/${id}`} className="flex flex-col gap-6 justify-between sm:flex-row">
+							<Image
+								src={imgSrc}
+								alt={`${username}_${title}`}
+								className="w-[150px] h-[100px] sm:w-[250px] sm:h-[166px]"
+								width={400}
+								height={200}
+								sizes={'(min-width: 640px): 250px, 150px'}
+							/>
+
 							<div className="">
-								<div className="justify-between mb-2 sm:flex">
+								<div className="justify-between items-center mb-2 sm:flex">
 									<div className="font-bold text-sm sm:text-[15px]">{title}</div>
 									<span className="px-2 py-1 text-xs font-semibold text-white bg-dark rounded-lg">
 										{username.includes('@') ? username.split('@')[0] : username}
