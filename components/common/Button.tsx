af571c41d779da6bffbaa96fb5cd8b13d8cc4959
olhkyle <kylekwon96@gmail.com>
2024-05-10 17:10:49 +0900

    [REFACTOR] add new Prop on Button UI

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -4,19 +4,29 @@ import { ReactNode } from 'react';
 
 interface ButtonProps {
 	type: 'button' | 'submit';
-	className: string;
+	className?: string;
 	onClick?: () => void;
 	disabled?: boolean;
 	children: ReactNode;
+	margin?: string;
+	padding?: string;
 }
 
-export default function Button({ type, className, disabled = false, onClick, children }: ButtonProps) {
+export default function Button({
+	type,
+	className,
+	disabled = false,
+	onClick,
+	children,
+	margin = '',
+	padding = 'py-[6px] px-[8px]',
+}: ButtonProps) {
 	return (
 		<button
 			type={type}
 			onClick={onClick}
 			disabled={disabled}
-			className={`${className} rounded-lg font-bold px-[12px] py-[6px]`}>
+			className={`${margin} ${padding} rounded-lg font-bold ${className}`}>
 			{children}
 		</button>
 	);
