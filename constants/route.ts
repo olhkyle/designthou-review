ffb552698681f1b4d359bb5b58440c21964d3d08
olhkyle <kylekwon96@gmail.com>
2024-05-14 21:39:23 +0900

    [FEAT] add new route on Route constant

--- constants/route.ts
+++ constants/route.ts
@@ -1,9 +1,12 @@
 const route = {
 	HOME: '/',
 	AUTH: '/auth',
+	ADMIN: '/admin',
 	MYPAGE: '/mypage',
 	CALENDAR: '/calendar',
 	REVIEWS: '/reviews',
+	WAREHOUSE: '/warehouse',
+	NEWS: '/news',
 } as const;
 
 export default route;
