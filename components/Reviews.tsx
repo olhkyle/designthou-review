ea16c08a14803ac64ae502f52a822d531cf19751
olhkyle <kylekwon96@gmail.com>
2024-05-14 21:39:35 +0900

    [REFAC] split Review Component

---
+++ components/Reviews.tsx
@@ -0,0 +1,41 @@
+import { createClient } from '@/supabase/client';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default async function Reviews() {
+	const supabase = createClient();
+	const { data: reviews } = await supabase.from('reviews').select('*');
+
+	return (
+		<ul className="flex flex-col gap-4 my-10">
+			{reviews?.slice(0, 9).map(({ id, username, title, content, imgSrc }) => (
+				<li
+					key={id}
+					className="px-4 py-4 border border-gray-200 rounded-xl outline-rose-200 outline-2 outline-offset-2 hover:outline active:outline cursor-pointer transition-all sm:px-6 md:px-8">
+					<Link href={`/reviews/${id}`} className="flex flex-col gap-8 justify-between sm:flex-row">
+						<Image
+							src={imgSrc}
+							alt={`${username}_${title}`}
+							className="w-[250px] sm:w-[400px] "
+							width={400}
+							height={266}
+							sizes={'(min-width: 640px): 400px, 250px'}
+						/>
+
+						<div className="">
+							<div className="justify-between items-center mb-2 sm:flex">
+								<div className="font-bold text-sm sm:text-[15px]">{title}</div>
+								<span className="px-2 py-1 text-xs font-semibold text-white bg-dark rounded-lg">
+									{username.includes('@') ? username.split('@')[0] : username}
+								</span>
+							</div>
+							<div className="text-sm text-ellipsis overflow-hidden break-keep whitespace-nowrap sm:text-[15px] sm:whitespace-normal">
+								{content}
+							</div>
+						</div>
+					</Link>
+				</li>
+			))}
+		</ul>
+	);
+}
