9d8e2e8c51c6bafccbdbf29b337b5c42fded9174
olhkyle <kylekwon96@gmail.com>
2024-05-14 21:31:01 +0900

    [CHORE] update supabase image source config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const SUPABASE_IMG_SOURCE_URL = 'ojedrtpbarwuhwbzthpe.supabase.co';
+
 const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
 				protocol: 'https',
 				hostname: 'designthou.com',
+			},
+			{
+				protocol: 'https',
+				hostname: SUPABASE_IMG_SOURCE_URL,
 			},
 		],
 	},
