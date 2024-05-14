a364d6124c44a35a5bd207f876e0975b35e134e0
olhkyle <kylekwon96@gmail.com>
2024-05-14 21:36:22 +0900

    ♻️ [REFAC] update CourseSelect Component

--- components/CourseSelect.tsx
+++ components/CourseSelect.tsx
@@ -1,5 +1,6 @@
 import course from '@/constants/course';
 import { Dispatch, SetStateAction } from 'react';
+import { MdClose } from 'react-icons/md';
 
 interface CourseSelectProps {
 	target: string;
@@ -9,26 +10,27 @@ interface CourseSelectProps {
 export default function CourseSelect({ target, setTarget }: CourseSelectProps) {
 	return (
 		<ul className="flex gap-2 px-1 pt-4 pb-2 w-full overflow-x-scroll">
-			{course.map(item => (
-				<li
-					key={item}
-					className={`px-2 py-1 h-[30px] text-sm border-[1px] ${
-						target === item
-							? 'bg-white border-rose-500 font-semibold'
-							: item === 'X'
-								? 'text-white bg-gray-800 border-white'
-								: 'bg-gray-300 border-white'
-					} rounded-md cursor-pointer`}
-					onClick={() => {
-						if (item === 'X') {
-							setTarget('');
-							return;
-						}
-						setTarget(item);
-					}}>
-					{item.toUpperCase()}
-				</li>
-			))}
+			{course.map(item =>
+				item === 'X' ? (
+					<li
+						key={item}
+						className="inline-flex justify-center items-center p-1 text-sm text-white border-[1px] border-white bg-gray-800 rounded-md cursor-pointer"
+						onClick={() => setTarget('')}>
+						<MdClose size={18} />
+					</li>
+				) : (
+					<li
+						key={item}
+						className={`inline-flex justify-center items-center px-2 py-1 h-[30px] text-sm border-[1px] ${
+							target === item ? 'bg-white border-rose-500 font-semibold' : 'bg-gray-300 border-white'
+						} rounded-md cursor-pointer`}
+						onClick={() => {
+							setTarget(item);
+						}}>
+						{item.toUpperCase()}
+					</li>
+				),
+			)}
 		</ul>
 	);
 }
