5db4ab989260ae7c3fb0169e5851aab1596f8c22
olhkyle <kylekwon96@gmail.com>
2024-05-17 18:55:46 +0900

    [FIX] fix Image Component's src missing issue

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,7 +1,8 @@
-import delay from '@/constants/delay';
 import { createClient } from '@/supabase/client';
 import Image from 'next/image';
 import Link from 'next/link';
+import DEFAULT_IMG_URL from '@/public/default_img.png';
+import blurDataUrl from '@/constants/blurDataUrl';
 
 export default async function Reviews() {
 	const supabase = createClient();
@@ -17,15 +18,17 @@ export default async function Reviews() {
 			{reviews?.slice(0, 9).map(({ id, username, title, content, imgSrc }) => (
 				<li
 					key={id}
-					className="px-4 py-4 border border-gray-200 rounded-xl outline-rose-200 outline-2 outline-offset-2 hover:outline active:outline cursor-pointer transition-all sm:px-6 md:px-8">
+					className="px-4 py-4 border border-gray-200 rounded-xl outline-rose-200 outline-2 outline-offset-2 hover:outline active:outline cursor-pointer transition-all sm:px-6 md:px-8 md:py-6">
 					<Link href={`/reviews/${id}`} className="flex flex-col gap-8 justify-between sm:flex-row">
 						<Image
-							src={imgSrc}
+							src={imgSrc ?? DEFAULT_IMG_URL}
 							alt={`${username}_${title}`}
-							className="w-[250px] sm:w-[400px] object-contain"
+							className="w-[300px] sm:w-[400px] object-contain"
 							width={400}
 							height={266}
-							sizes={'(min-width: 640px): 400px, 250px'}
+							sizes={'(min-width: 640px): 400px, 300px'}
+							placeholder="blur"
+							blurDataURL={blurDataUrl}
 						/>
 
 						<div className="">
---
+++ constants/blurDataUrl.ts
@@ -0,0 +1,4 @@
+const blurDataUrl =
+	'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==';
+
+export default blurDataUrl;
