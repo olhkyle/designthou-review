cc383758ff46298876502ca0ab6aab18f5a9238a
olhkyle <kylekwon96@gmail.com>
2024-05-20 19:01:32 +0900

    [STYLE] update ReviewsLoading ui style

--- components/loader/ReviewsLoading.tsx
+++ components/loader/ReviewsLoading.tsx
@@ -1,10 +1,10 @@
 export default function ReviewsLoading() {
 	return (
 		<ul className="flex flex-col gap-4 my-10 w-full">
-			<li className="px-4 py-4 w-full h-[150px] rounded-xl bg-gray-50 animate-pulse sm:px-6 md:px-8"></li>
-			<li className="px-4 py-4 w-full h-[150px] rounded-xl bg-gray-50 animate-pulse sm:px-6 md:px-8"></li>
-			<li className="px-4 py-4 w-full h-[150px] rounded-xl bg-gray-50 animate-pulse sm:px-6 md:px-8"></li>
-			<li className="px-4 py-4 w-full h-[150px] rounded-xl bg-gray-50 animate-pulse sm:px-6 md:px-8"></li>
+			<li className="px-4 py-4 w-full h-[150px] loading-pulse sm:px-6 md:px-8"></li>
+			<li className="px-4 py-4 w-full h-[150px] loading-pulse sm:px-6 md:px-8"></li>
+			<li className="px-4 py-4 w-full h-[150px] loading-pulse sm:px-6 md:px-8"></li>
+			<li className="px-4 py-4 w-full h-[150px] loading-pulse sm:px-6 md:px-8"></li>
 		</ul>
 	);
 }
