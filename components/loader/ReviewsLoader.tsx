a21a315ae4b2e74ef5fc92be7f33f31f1fc9216e
olhkyle <kylekwon96@gmail.com>
2024-05-20 19:02:57 +0900

    [FEAT] update Loader's name with style

--- components/loader/ReviewsLoader.tsx
+++ components/loader/ReviewsLoader.tsx
@@ -1,4 +1,4 @@
-export default function ReviewsLoading() {
+export default function ReviewsLoader() {
 	return (
 		<ul className="flex flex-col gap-4 my-10 w-full">
 			<li className="px-4 py-4 w-full h-[150px] loading-pulse sm:px-6 md:px-8"></li>
